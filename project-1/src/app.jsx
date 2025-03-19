831daf1858d69c5de94a76267209f0ddf08b432e
Nitishnk24 <nitishnkotumuchagi@gmail.com>
2025-03-19 09:03:07 +0530

    initial commit

---
+++ project-1/src/app.jsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+import "./components/navigation";
+import Navigation from "./components/navigation";
+import Herosection from "./components/hero";
+
+
+
+const App = () => {
+    return (
+      <div>
+      <Navigation/>
+      <Herosection/>
+      </div>
+    );
+  };
+  
+  export default App;
---
+++ project-1/src/components/hero.jsx
@@ -0,0 +1,26 @@
+const Herosection=()=> {
+    return<main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p> YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.</p>
+          <div className="hero-btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+          </div>
+          <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-name">
+          < img src ="/images/amazon.png" alt="logo" onClick="https://amzn.in/d/6apccgG"/>
+          < img src ="/images/flipkart.png" alt="logo"/>
+          </div>
+          </div>
+        </div>
+        <div className="hero-img">
+        < img src ="/images/hero-image.png" alt="logo"/>
+        </div>
+    </main>
+};
+
+export default Herosection;
